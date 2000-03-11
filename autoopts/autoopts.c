23bf1232ff0d12793c2491c6c31a1ed90da647c4
bruce <bruce>
2000-03-11 18:31:18 +0000

    Remove tabs

--- autoopts/autoopts.c
+++ autoopts/autoopts.c
@@ -1,6 +1,6 @@
 
 /*
- *  $Id: autoopts.c,v 2.10 1999/09/08 22:08:44 bkorb Exp $
+ *  $Id: autoopts.c,v 2.11 2000/03/11 18:31:18 bruce Exp $
  *
  *  This file contains all of the routines that must be linked into
  *  an executable to use the generated option processing.  The optional
@@ -1080,69 +1080,69 @@ checkConsistency( tOptions* pOpts, int argCt )
      *  THEN it is time to call usage exit.
      */
     if (errCt != 0)
-	(*pOpts->pUsageProc)( pOpts, EXIT_FAILURE );
+        (*pOpts->pUsageProc)( pOpts, EXIT_FAILURE );
 
     /*
      *  FOR each of "oCt" options, ...
      */
     for (;;) {
-	tSCC zNotEnough[] = "ERROR:  The %s option must appear %d times\n";
-	tSCC zNeedOne[]   = "ERROR:  The %s option is required\n";
+        tSCC zNotEnough[] = "ERROR:  The %s option must appear %d times\n";
+        tSCC zNeedOne[]   = "ERROR:  The %s option is required\n";
 
-	const int*  pMust = pOD->pOptMust;
-	const int*  pCant = pOD->pOptCant;
+        const int*  pMust = pOD->pOptMust;
+        const int*  pCant = pOD->pOptCant;
 
-	/*
-	 *  IF the current option was provided on the command line
-	 *  THEN ensure that any "MUST" requirements are not
-	 *       "DEFAULT" (unspecified) *AND* ensure that any
-	 *       "CANT" options have not been SET or DEFINED.
-	 */
-	if (SELECTED_OPT(pOD)) {
-	    if (pMust != (const int*)NULL) for (;;) {
-		tOptDesc*  p = pOpts->pOptDesc + *(pMust++);
-		if (UNUSED_OPT(p)) {
-		    const tOptDesc* pN = pOpts->pOptDesc + pMust[-1];
-		    errCt++;
-		    fprintf( stderr, zReqFmt, pOD->pz_Name, pN->pz_Name );
-		}
-		
-		if (*pMust == NO_EQUIVALENT)
-		    break;
-	    }
-	    
-	    if (pCant != (const int*)NULL) for (;;) {
-		tOptDesc*  p = pOpts->pOptDesc + *(pCant++);
-		if (SELECTED_OPT(p)) {
-		    const tOptDesc* pN = pOpts->pOptDesc + pCant[-1];
-		    errCt++;
-		    fprintf( stderr, zCantFmt, pOD->pz_Name, pN->pz_Name );
-		}
+        /*
+         *  IF the current option was provided on the command line
+         *  THEN ensure that any "MUST" requirements are not
+         *       "DEFAULT" (unspecified) *AND* ensure that any
+         *       "CANT" options have not been SET or DEFINED.
+         */
+        if (SELECTED_OPT(pOD)) {
+            if (pMust != (const int*)NULL) for (;;) {
+                tOptDesc*  p = pOpts->pOptDesc + *(pMust++);
+                if (UNUSED_OPT(p)) {
+                    const tOptDesc* pN = pOpts->pOptDesc + pMust[-1];
+                    errCt++;
+                    fprintf( stderr, zReqFmt, pOD->pz_Name, pN->pz_Name );
+                }
+                
+                if (*pMust == NO_EQUIVALENT)
+                    break;
+            }
+            
+            if (pCant != (const int*)NULL) for (;;) {
+                tOptDesc*  p = pOpts->pOptDesc + *(pCant++);
+                if (SELECTED_OPT(p)) {
+                    const tOptDesc* pN = pOpts->pOptDesc + pCant[-1];
+                    errCt++;
+                    fprintf( stderr, zCantFmt, pOD->pz_Name, pN->pz_Name );
+                }
 
-		if (*pCant == NO_EQUIVALENT)
-		    break;
-	    }
-	}
+                if (*pCant == NO_EQUIVALENT)
+                    break;
+            }
+        }
 
-	/*
-	 *  IF       this option is not equivalenced to another,
-	 *        OR it is equivalenced to itself (is the equiv. root)
-	 *    AND it does not occur often enough
-	 *  THEN note the error
-	 */
-	if (  (  (pOD->optEquivIndex == NO_EQUIVALENT)
-	      || (pOD->optEquivIndex == pOD->optIndex) )
+        /*
+         *  IF       this option is not equivalenced to another,
+         *        OR it is equivalenced to itself (is the equiv. root)
+         *    AND it does not occur often enough
+         *  THEN note the error
+         */
+        if (  (  (pOD->optEquivIndex == NO_EQUIVALENT)
+              || (pOD->optEquivIndex == pOD->optIndex) )
            && (pOD->optOccCt <  pOD->optMinCt)  )  {
 
-	    errCt++;
-	    if (pOD->optMinCt > 1)
-		fprintf( stderr, zNotEnough, pOD->pz_Name, pOD->optMinCt );
-	    else fprintf( stderr, zNeedOne, pOD->pz_Name );
-	}
+            errCt++;
+            if (pOD->optMinCt > 1)
+                fprintf( stderr, zNotEnough, pOD->pz_Name, pOD->optMinCt );
+            else fprintf( stderr, zNeedOne, pOD->pz_Name );
+        }
 
-	if (--oCt <= 0)
-	    break;
-	pOD++;
+        if (--oCt <= 0)
+            break;
+        pOD++;
     }
 
     /*
@@ -1241,7 +1241,7 @@ optionProcess( tOptions*  pOpts, int argCt, char** argVect )
      *  THEN look for too few occurrences of required options
      */
     if ((pOpts->fOptSet & OPTPROC_ERRSTOP) != 0)
-	checkConsistency( pOpts, argCt );
+        checkConsistency( pOpts, argCt );
 
     return pOpts->curOptIdx;
 }
