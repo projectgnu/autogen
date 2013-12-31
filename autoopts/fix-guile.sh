352601ab0156efd28bfadeec9f605e740569d54f
Bruce Korb <bkorb@gnu.org>
2013-12-31 11:24:05 -0800

    checkpoint status

--- autoopts/fix-guile.sh
+++ autoopts/fix-guile.sh
@@ -1,44 +1,61 @@
 #! /bin/false
 
-guile_scm_h=
+scan_args() {
+    libguiledir=
+    while test $# -gt 0
+    do
+        case "$1" in
+        -I )
+            test -f $2/libguile/__scm.h && {
+                libguiledir=$2
+                return 0
+            }
+            ;;
+        -I* )
+            f=${1#-I}
+            test -f $f/libguile/__scm.h && {
+                libguiledir=$f
+                return 0
+            }
+            ;;
+        esac
+        shift
+    done
 
-while test $# -gt 0
-do
-    case "$1" in
-    -I )
-        test -f $2/libguile/__scm.h && {
-            guile_scm_h=$2/libguile/__scm.h
-            break
-        }
-        ;;
-    -I* )
-        f=${1#-I}
-        test -f $f/libguile/__scm.h && {
-            guile_scm_h=$f/libguile/__scm.h
-            break
-        }
-        ;;
-    esac
-    shift
-done
-
-test -z "$guile_scm_h" && {
-    guile_scm_h=/usr/include/libguile/__scm.h
-    test -f $guile_scm_h || {
-        echo "The Guile header __scm.h cannot be found"
-        exit 1
-    } 1>&2
+    libguiledir=/usr/include
+    test -f $libguiledir/libguile/__scm.h && return 0
+    echo "The Guile header __scm.h cannot be found" 1>&2
+    exit 1
 }
 
-grep -E $'/^#define[ \t]SCM_NORETURN.*\(noreturn\)'$guile_scm_h >/dev/null || \
-    exit 0
+find_libguiledir() {
+    guile_scm_h=
+    libguiledir=`exec 2>/dev/null ; guile-config info includedir`
+    test -d "$libguiledir" || scan_args "$@"
+
+    guile_scm_h=${libguiledir}/libguile/__scm.h
+}
+
+find_libguiledir "$@"
+
+list=`find ${libguiledir}/libguile* -type f`
+list=`grep -l -E '\<noreturn\>' $list`
+
+test -z "$list" && exit 0
 
 test -d libguile || mkdir libguile || {
     echo "cannot make libguile directory"
     exit 1
 } 1>&2
 
-set -e
-sed $'/^#define[ \t]SCM_NORETURN.*(noreturn)/s@(noreturn)@(__noreturn__)@' \
-    $guile_scm_h > libguile/__scm.h
-cp ${guile_scm_h%/__scm.h}.h .
+noret='\([^a-zA-Z0-9_]\)noreturn\([^a-zA-Z0-9_]\)'
+nores='\1__noreturn__\2'
+sedex="s@${noret}@${nores}@"
+
+for f in $list
+do
+    g=libguile${f##*/libguile}
+    sed "${sedex}" $f > $g
+done
+
+test -f libguile.h || cp ${libguiledir}/libguile.h .
