9fd32a85c05242d375f0832185c9accf59993fc2
Bruce Korb <bruce.korb@hgst.com>
2015-07-18 17:48:56 -0700

    simplify building char-mapper for linux

--- add-on/char-mapper/mk-opt-table.sh
+++ add-on/char-mapper/mk-opt-table.sh
@@ -18,6 +18,7 @@
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 declare -r progdir=$(\cd $(dirname $0) && pwd -P)
+declare -r prog=$(basename $0 .sh)
 declare -r prognam=$(basename $0)
 declare -r program=${progdir}/${prognam}
 
@@ -28,8 +29,8 @@ die() {
 } >&2
 
 init() {
-    base_name=${1%%.*}
     exe_name=char-mapper
+    cd ${progdir}
     set -e
 
     declare -g hdl_list=$(
@@ -38,6 +39,13 @@ init() {
             sort)
 
     declare -g new_source=false
+    agexe=$(command -v autogen)XXX
+    test -x ${agexe} || {
+        tar -xf cm.tar
+        touch $(tar -tf cm.tar)
+        new_source=true
+        unset agexe
+    }
 }
 
 is_up_to_date() {
@@ -57,29 +65,19 @@ is_up_to_date() {
 }
 
 mk_enum() {
-    autogen -L ../../autoopts/tpl <<- _EOF_
-	AutoGen Definitions str2enum;
-
-	base-name = ${base_name};
-	length    = none;
-	no-case; no-name;
-	dispatch = {
-	    d-ret = 'char *';
-	    d-nam = 'handle_%s';
-	};
-	cmd    = $(echo $hdl_list | sed 's/ /, /g');
-	_EOF_
+    test ${#agexe} -gt 0 || return 0
+    def=$(mktemp ./cm-opt-XXXXXX)
+    {
+        sed $'1,/^## *BEGIN *CM-OPT/d;/^## *END *CM-OPT/,$d;s/^\t//' ${program}
+        echo $hdl_list';' | sed 's/ /, /g'
+    } > ${def}
+    ${agexe} -L ${progdir}../../autoopts/tpl ${def} || die "autogen failure"
+    rm -f ${def}
 }
 
 assemble_usage() {
-    cat <<- _EOF_
-	str = <<_EOUsage_
-	USAGE:  ${exe_name} [ -h | --help | <input-file> ]
-	If the '<input-file>' is not specified, it is read from standard input.
-	Input may not be from a TTY device.  Various directives affecting the
-	output are embedded in the input text.  These are:
-
-	_EOF_
+    sed '1,/^## *BEGIN *USE-HEAD/d;/^## *END *USE-HEAD/,$d
+	s/${exe_name}/'${exe_name}'/;' ${program}
 
     for d in $hdl_list
     do
@@ -95,7 +93,55 @@ assemble_usage() {
             fmt
     done
 
-    cat <<- _EOF_
+    sed '1,/^## *BEGIN *USAGE/d;/^## *END *USAGE/,$d' ${program}
+}
+
+mk_text_def() {
+    is_up_to_date map-text ${program} && return 0
+    test ${#agexe} -gt 0 || return 0
+    echo "updating map-text.[ch]"
+
+    # pull the name of the included file from the def file and then
+    # put our assembled text into that file.
+    #
+    declare tmp_text=$(awk '/^#include/{print $2}' map-text.def)
+    assemble_usage > $tmp_text
+    ${agexe} map-text.def
+    rm -f $tmp_text
+}
+
+trap "die failure" EXIT
+stamp_file=${1}
+temp_stamp=${1}-temp
+date > ${temp_stamp}
+
+init "$1"
+mk_enum
+mk_text_def
+
+if ${new_source}
+then mv -f ${temp_stamp} ${stamp_file}
+else rm -f ${temp_stamp}
+     echo no changes to generated source
+fi
+
+trap '' EXIT
+exit 0
+
+## BEGIN CM-OPT
+	AutoGen Definitions str2enum;
+
+	base-name = cm-opt;
+	length    = none;
+	no-case; no-name;
+	dispatch = {
+	    d-ret = 'char *';
+	    d-nam = 'handle_%s';
+	};
+	cmd =
+## END CM-OPT
+
+## BEGIN USAGE
 	Otherwise, input lines that are blank or start with a hash ('#') are
 	ignored.  All other lines must conform to the following syntax:
 
@@ -121,45 +167,14 @@ assemble_usage() {
 	also be inserted into the output as a comment.
 
 	_EOUsage_;
-	_EOF_
-}
+## END USAGE
 
-mk_text_def() {
-    is_up_to_date map-text ${program} && return 0
+## BEGIN USE-HEAD
+	str = <<- _EOUsage_
+	USAGE:  ${exe_name} [ -h | --help | <input-file> ]
+	If the '<input-file>' is not specified, it is read from standard input.
+	Input may not be from a TTY device.  Various directives affecting the
+	output are embedded in the input text.  These are:
 
-    echo "updating map-text.[ch]"
-    declare ag=$(command -v autogen)
-    test -x "$ag" || {
-        test -f map-text.c -a -f map-text.h || \
-            die "map-text.c and .h are missing and cannot be rebuilt"
-        echo "warning: map-text.c and .h are out of date and cannot be rebuilt"
-        touch map-text.c map-text.h
-        return 0
-    } 1>&2
-
-    # pull the name of the included file from the def file and then
-    # put our assembled text into that file.
-    #
-    declare tmp_text=$(awk '/^#include/{print $2}' map-text.def)
-    assemble_usage > $tmp_text
-    ${ag} map-text.def
-    rm -f $tmp_text
-}
-
-trap "die failure" EXIT
-stamp_file=${1}
-temp_stamp=${1}-temp
-date > ${temp_stamp}
-
-init "$1"
-mk_enum
-mk_text_def
-
-if ${new_source}
-then mv -f ${temp_stamp} ${stamp_file}
-else rm -f ${temp_stamp}
-     echo no changes to generated source
-fi
-
-trap '' EXIT
-exit 0
+## END USE-HEAD
+_EOUsage_
